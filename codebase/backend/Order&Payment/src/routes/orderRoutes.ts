3d3b6f5c7f93fa0701561367f905c6eae3cfd695
Disira Thihan <87310369+it21070358@users.noreply.github.com>
2023-04-19 14:21:35 +0530

    Update backend file structure

--- codebase/backend/Order&Payment/src/routes/orderRoutes.ts
+++ codebase/backend/Order&Payment/src/routes/orderRoutes.ts
@@ -4,8 +4,8 @@ import {
   getMyOrders,
   getOrderById,
   getOrders,
-  updateOrderToDelivered,
-  updateOrderToPaid
+  // updateOrderToDelivered,
+  // updateOrderToPaid
 } from "../controllers/orderController";
 // import { admin, protect } from "../middleware/authMiddleware";
 
@@ -14,7 +14,7 @@ const router = express.Router();
 router.route("/").post(/*protect, */addOrderItems).get(/*protect, admin,*/ getOrders);
 router.route("/myorders").get(/*protect, */getMyOrders);
 router.route("/:id").get(/*protect,*/ getOrderById);
-router.route("/:id/pay").put(/*protect,*/ updateOrderToPaid);
-router.route("/:id/deliver").put(/*protect, admin, */updateOrderToDelivered);
+// router.route("/:id/pay").put(/*protect,*/ updateOrderToPaid);
+// router.route("/:id/deliver").put(/*protect, admin, */updateOrderToDelivered);
 
 export default router;
